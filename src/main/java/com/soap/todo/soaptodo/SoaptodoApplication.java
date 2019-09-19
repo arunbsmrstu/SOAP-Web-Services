2c45ce2aea1230735e4cdda7d2be0c35adc61bb5
arun <arunbsmrstu@gmail.com>
2019-09-19 20:16:10 +0600

    o

---
+++ src/main/java/com/soap/todo/soaptodo/SoaptodoApplication.java
@@ -0,0 +1,13 @@
+package com.soap.todo.soaptodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaptodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaptodoApplication.class, args);
+	}
+
+}
